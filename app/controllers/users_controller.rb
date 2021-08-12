f0ad2700913d668733ee0a3a5986d68d49201280
Rahul Saw <rahul.saw@anchanto.com>
2021-08-12 16:26:40 +0530

    Alpha Blog :: Authentication : Login Page updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.require(:user).permit(:username, :email, :password)
   end
 
   def set_user
